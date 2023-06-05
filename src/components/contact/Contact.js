919319c3ee8076ba9bab8435896695e68388ab45
mantu kumar yadav <mantukr446@gmail.com>
2023-06-05 13:06:35 +0530

    first commit

---
+++ src/components/contact/Contact.js
@@ -0,0 +1,55 @@
+import React from "react"
+import Back from "../common/back/Back"
+import "./contact.css"
+
+const Contact = () => {
+  const map = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d57267.10198456923!2d84.3186206928344!3d26.222889378657502!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3992fc0166e38983%3A0x623c6c1cb7d5211f!2sSiwan%2C%20Bihar!5e0!3m2!1sen!2sin!4v1685882858696!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" '
+  return (
+    <>
+      <Back title='Contact us' />
+      <section className='contacts padding'>
+        <div className='container shadow flexSB'>
+          <div className='left row'>
+            <iframe src={map}></iframe>
+          </div>
+          <div className='right row'>
+            <h1>Contact us</h1>
+            <p>We're open for any suggestion or just to have a chat</p>
+
+            <div className='items grid2'>
+              <div className='box'>
+                <h4>ADDRESS:</h4>
+                <p>198 West 21th Street, Siwan, Bihar, 101214</p>
+              </div>
+              <div className='box'>
+                <h4>EMAIL:</h4>
+                <p> info@yoursite.com</p>
+              </div>
+              <div className='box'>
+                <h4>PHONE:</h4>
+                <p> +91 1235 2355 98</p>
+              </div>
+            </div>
+
+            <form action=''>
+              <div className='flexSB'>
+                <input type='text' placeholder='Name' />
+                <input type='email' placeholder='Email' />
+              </div>
+              <input type='text' placeholder='Subject' />
+              <textarea cols='30' rows='10'>
+                Create a message here...
+              </textarea>
+              <button className='primary-btn'>SEND MESSAGE</button>
+            </form>
+
+            <h3>Follow us here</h3>
+            <span>FACEBOOK TWITTER INSTAGRAM DRIBBBLE</span>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Contact
